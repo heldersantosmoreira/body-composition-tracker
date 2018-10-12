daebbb2a3f542e749f4f4240371b2763bdcc58a7
Hélder Moreira <heldersantos.moreira@gmail.com>
2018-10-12 17:56:31 +0100

    Refactor landing page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    latest = WeighIn.order(when: :desc).first
+    first = WeighIn.order(when: :asc).first
+
+    distance = first.weight - StatsHelper::WEIGHT_GOAL
+    current_distance = latest.weight - StatsHelper::WEIGHT_GOAL
+
+    @progress = latest.present? ? (current_distance * 100 / distance).to_i  : nil
   end
 end
--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,4 +1,3 @@
 module StatsHelper
   WEIGHT_GOAL = 96
-  FAT_GOAL = 14
 end

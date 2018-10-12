7eaa9cad2f3d000644ad85455cf1d7cf2feaaa84
Hélder Moreira <heldersantos.moreira@gmail.com>
2018-10-12 09:40:27 +0100

    Refactor stats page
    - optimize queries
    - add more info (latest, lowest, starting)

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,22 +1,27 @@
 class StatsController < ApplicationController
   def index
-    weigh_ins = WeighIn.order(when: :asc).all
+    @weigh_ins = WeighIn.order(when: :asc).all
+    @starting = @weigh_ins.first
+    @lowest = WeighIn.order(weight: :asc).first
+    @latest = WeighIn.order(when: :desc).first
 
-    timeline = (weigh_ins.first.when.to_datetime .. weigh_ins.last.when.to_datetime + 1).map do |date|
-      [
-        date,
-        weigh_ins.find { |weigh_in| weigh_in.when.to_datetime.to_date == date.to_date }
-      ]
+    if @starting.present? && @latest.present?
+      timeline = (@starting.when.to_datetime .. @latest.when.to_datetime).map do |date|
+        [
+          date,
+          @weigh_ins.find { |weigh_in| weigh_in.when.to_datetime.to_date == date.to_date }
+        ]
+      end
+
+      @rolling_7day_average = timeline.each_slice(7).map do |slice|
+        values = slice.map(&:last).compact
+        {
+          weight_average: values.inject(0) { |sum, el| sum + el.weight }.to_f / values.size,
+          fat_average: values.inject(0) { |sum, el| sum + el.fat }.to_f / values.size,
+          date_range: [slice.last.first, slice.first.first],
+          size: values.size
+        }
+      end.reverse
     end
-
-    @rolling_7day_average = timeline.each_slice(7).map do |slice|
-      values = slice.map(&:last).compact
-      {
-        weight_average: values.inject(0) { |sum, el| sum + el.weight }.to_f / values.size,
-        fat_average: values.inject(0) { |sum, el| sum + el.fat }.to_f / values.size,
-        date_range: [slice.last.first, slice.first.first],
-        size: values.size
-      }
-    end.reverse
   end
 end
